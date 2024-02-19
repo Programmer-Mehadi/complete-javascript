29e9a4a13707a843e3f266c8693aee7a77d34b02
Md Zahirul Islam Mehadi <108610227+Programmer-Mehadi@users.noreply.github.com>
2024-02-19 16:07:24 +0600

    for in done.

---
+++ Loop/for-in.js
@@ -0,0 +1,16 @@
+let numbers = [1, 2, 3, 4, 5]
+
+for (let i in numbers) {
+  console.log(numbers[i])
+}
+
+// for object
+
+let obj = {
+  name: "Mehadi",
+  age: 25,
+}
+
+for (const data in obj) {
+  console.log(obj[data])
+}
