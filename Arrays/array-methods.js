0a0f29ced53d35e4e674f59be8e58fb51116b0f7
Md Zahirul Islam Mehadi <108610227+Programmer-Mehadi@users.noreply.github.com>
2024-02-19 15:39:07 +0600

    array-methods

---
+++ Arrays/array-methods.js
@@ -0,0 +1,19 @@
+let numbers = [1, 2, 3, 4, 5]
+
+console.log(numbers)
+
+console.log(numbers.join(", "))
+
+console.log(numbers.slice(2))
+
+console.log(numbers.slice(2, 4))
+
+console.log(numbers.splice(2, 1))
+
+console.log(numbers)
+
+console.log(numbers.sort())
+
+console.log(numbers.reverse())
+
+console.log(numbers)
