f2a52f4357449141e1ebef8a794ef2521d25f837
Md Zahirul Islam Mehadi <108610227+Programmer-Mehadi@users.noreply.github.com>
2024-02-19 15:37:17 +0600

    array nbasic.

---
+++ Arrays/basic.js
@@ -0,0 +1,27 @@
+let numbers = [1, 2, 3, 4, 5]
+
+console.log(numbers)
+
+numbers.push(6)
+
+console.log(numbers)
+
+console.log(numbers.length)
+
+console.log(numbers[3])
+
+console.log(numbers[numbers.length - 1])
+
+console.log(numbers[numbers.length - 2])
+
+numbers.pop()
+
+console.log(numbers)
+
+numbers.shift()
+
+console.log(numbers)
+
+numbers.unshift(10)
+
+console.log(numbers)
