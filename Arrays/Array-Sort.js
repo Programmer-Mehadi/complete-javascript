c2bf02716605e79a46965393031eee6536a45af6
Md Zahirul Islam Mehadi <108610227+Programmer-Mehadi@users.noreply.github.com>
2024-02-19 15:54:50 +0600

    Array Sort

---
+++ Arrays/Array-Sort.js
@@ -0,0 +1,9 @@
+let numbers = [1, 2, 3, 4, 567, 435, 23, 78, 23]
+
+console.log(numbers.join(", "))
+
+console.log(numbers.sort())
+
+console.log(numbers.sort((a, b) => a - b))
+
+console.log(numbers.sort((a, b) => b - a))
