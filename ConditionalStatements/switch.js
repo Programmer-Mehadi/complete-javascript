703521ab0df649dc22076f0f1be8bd80a65bd8a9
Md Zahirul Islam Mehadi <108610227+Programmer-Mehadi@users.noreply.github.com>
2024-02-19 16:04:23 +0600

    switch done.

---
+++ ConditionalStatements/switch.js
@@ -0,0 +1,13 @@
+let num = 45
+
+switch (num % 2) {
+  case 1:
+    console.log("Odd")
+    break
+  case 0:
+    console.log("Even")
+    break
+  default:
+    console.log("Invalid")
+    break
+}
